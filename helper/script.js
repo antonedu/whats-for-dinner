9f802ae76a460f562fc4897dc7b19536a0a2e039
antonedu <55575226+antonedu@users.noreply.github.com>
2021-04-20 14:48:47 +0200

    Started on consent message and added ability to add dishes

--- helper/script.js
+++ helper/script.js
@@ -1,10 +1,34 @@
 var dishes = []
 var localStorageActivated;
+var cookieAsked = false;
+class dish {
+  constructor(name, weekdays, dates, freq) {
+    this.name = name;
+    this.weekdays = weekdays;
+    this.dates = dates;
+    this.freq = freq;
+  }
+}
 
 if (localStorage.getItem("dishes") === null) {
   localStorageActivated = false;
 }
 else {
   localStorageActivated = true;
-  dishes = localStorage.getItem("dishes");
+  dishes = JSON.parse(localStorage.getItem("dishes"));
+}
+
+function askForCookies(consented) {
+  cookieAsked = true;
+  if (consented === true) {
+    localStorageActivated = true;
+  }
+}
+
+function addDish(name, weekdays, dates, freq) {
+  let newDish = new dish(name, weekdays, dates, freq);
+  dishes.push(newDish);
+  if (localStorageActivated === true) {
+    localStorage.setItem("dishes", JSON.stringify(dishes))
+  }
 }
