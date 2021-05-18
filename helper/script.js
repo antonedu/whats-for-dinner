f2b2c780c9b4e888e89f9690ed8c9f923ba5f890
antonedu <55575226+antonedu@users.noreply.github.com>
2021-05-18 13:04:06 +0200

    Started tweaking second version of the alogrithm

--- helper/script.js
+++ helper/script.js
@@ -148,7 +148,7 @@ function randomizeDishOrder() {
     randomDishArray[randomIndex] = temporaryValue;
   }
   for (let i = 0; i<randomDishArray.length;i++) {
-    randomDishArray[i].sinceLast = 0;
+    randomDishArray[i].sinceLast = 1;
   }
 }
 
@@ -156,84 +156,37 @@ function generateMenu(amount) {
   let index;
   let currentDay = new Date().getDay()-1;
   let startLength = menu.length;
-  let temp;
-  let aocm; // array of current matches
+  let awwv = []; // Array with weighted values
+  let weekmatchvalue;
   if (currentDay < 0) {
     currentDay = 6;
   }
   for (let i = 0; i < amount; i++) {
-    index = -1;
+    index = 0;
     // Algorythm for getting a new dishes
-    aocm = randomDishArray;
-    // matchar de som inte varit på länge
-    temp = aocm.filter(dish => Math.log(dish.freq*2+1)*dish.sinceLast/10 > 3*Math.log(dishes.length));
-    if (temp.length != 0) {
-      aocm = temp;
-      temp = aocm.filter(dish => dish.weekdays[currentDay] === true);
-      if (temp.length != 0) {
-        // matchar de som inte varit på länge och som matchar veckodagen
-        aocm = temp;
-        console.log("case 1")
+    for (let j = 0; j < randomDishArray.length; j++) {
+      if (randomDishArray[j].weekdays === 0) {
+        weekdaymatchvalue = 2;
+      } else if (randomDishArray[j].weekdays[currentDay] === true) {
+        weekdaymatchvalue = 4;
       } else {
-        temp = aocm.filter(dish => Arry.isArray(dish.weekdays) === false);
-        if (temp.length != 0) {
-          // matchar de som inte varit på länge och som inte har bestämd veckodag
-          aocm = temp;
-          console.log("case 2")
-        }
+        weekdaymatchvalue = 1;
       }
-    } else {
-      // matchar de som inte varit på kortare tid
-      temp = aocm.filter(dish => Math.log(dish.freq*2+1)*dish.sinceLast/10 > 1.3*Math.log(dishes.length));
-      if (temp.length != 0) {
-        aocm = temp;
-        temp = aocm.filter(dish => dish.weekdays[currentDay] === true);
-        if (temp.length != 0) {
-          // matchar de som inte varit på kortare tid och som matchar veckodagen
-          aocm = temp;
-          console.log("case 3")
-        } else {
-          temp = aocm.filter(dish => dish.weekdays == 0);
-          if (temp.length != 0) {
-            // matchar de som inte varit på en kortare tid och som inte har en veckodag
-            aocm = temp;
-            console.log("case 4")
-          }
-        }
-      } else {
-        temp = aocm.filter(dish => dish.weekdays[currentDay] === true);
-        if (temp.length != 0) {
-          // matchar som har matchande veckodagar
-          aocm = temp;
-          console.log("case 5")
-        } else {
-          temp = aocm.filter(dish => dish.weekdays === 0);
-          if (temp.length != 0) {
-            // matchar som inte har någon veckodag
-            aocm = temp;
-            console.log("case 6")
-          }
-        }
+      awwv[j] = {
+        index: j,
+        weighedvalue: randomDishArray[j].sinceLast * weekdaymatchvalue * randomDishArray[j].freq
       }
     }
-    if (aocm.length != 0) {
-      let i = 0;
-      while (index < 0 && i < randomDishArray.length) {
-        if (randomDishArray[i].name === aocm[0].name) {
-          index = i;
-        }
-        i++;
+    for (let j = 0; j < randomDishArray.length; j++) {
+      if (awwv[j].weighedvalue > awwv[index].weighedvalue) {
+        index = j;
       }
-    } else {
-      // matchar den första i arrayen om det inte finns andra matcher
-      index = 0;
-      console.log("case 7")
     }
-    menu[i] = randomDishArray[index];
+    menu[i] = randomDishArray[awwv[index].index];
     for (let i = 0; i < randomDishArray.length;i++) {
       randomDishArray[i].sinceLast++;
     }
-    randomDishArray[index].sinceLast = 0;
+    randomDishArray[index].sinceLast = 1;
     randomDishArray.push(randomDishArray.splice(index,1)[0]);
     currentDay++;
     if (currentDay > 6) {
