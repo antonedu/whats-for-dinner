9ef43bad1c02cd7a95550b08102ec85744ef26b0
antonegas <32789589+antonegas@users.noreply.github.com>
2022-03-23 10:08:10 +0100

    Moved Dish class to its own module

--- src/dishesFunctions.js
+++ src/dishesFunctions.js
@@ -1,51 +1,6 @@
 import "./ISO8601dates.js";
 import { generateUniqueID, shuffleArray } from "./utilityFunctions.js"
-
-class Dish {
-    // Dish class
-    constructor(name, weekdays, dates, freq, id, lastSeen = null) {
-        this.name = name;
-        this.weekdays = weekdays;
-        this.dates = dates;
-        this.freq = freq;
-        this.id = id;
-        this.lastSeen = lastSeen;
-    };
-
-    get weekdaysStr() {
-        // Returns str with active weekdays
-        let weekdaysStr = "";
-        const WEEKDAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-        if (this.allWeekdays()) {
-            if (this.weekdays[0] === true) {
-                return "all";
-            } else {
-                return "any";
-            }
-        };
-        for (let i = 0; i < 7; i++) {
-            if (this.weekdays[i] == true) {
-                if (weekdaysStr.length == 0) {
-                    weekdaysStr += WEEKDAYS[i];
-                } else {
-                    weekdaysStr += ", " + WEEKDAYS[i];
-                };
-            };
-        };
-        return weekdaysStr
-    };
-
-    allWeekdays() {
-        // If all days are true/all days are false return true else false
-        if (this.weekdays.every((day, index, arr) => {
-            return day == arr[0];
-        })) {
-            return true;
-        } else {
-            return false;
-        };
-    }
-};
+import Dish from "./DishClass.js"
 
 export function setCookies(consented, dishes, menu) {
     // Sets wether cookies are activated or not based on [consented]
