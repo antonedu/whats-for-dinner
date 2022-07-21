d09f4a0a048e3cf4598e1f3a0f2650d4c17daad3
antonegas <32789589+antonegas@users.noreply.github.com>
2022-07-21 11:35:54 +0200

    Removed unused fields

--- src/DishClass.ts
+++ src/DishClass.ts
@@ -1,26 +1,18 @@
-import ISODate from "./ISO8601dates";
-import { generateID } from "./utilityFunctions";
-
 export type Weekdays = [boolean, boolean, boolean, boolean, boolean, boolean, boolean, boolean];
 export type Freq = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export default class Dish {
     name: string;
     weekdays: Weekdays;
-    dates?: ISODate[];
     freq: Freq;
-    id: string;
-    lastSeen?: ISODate;
     // Dish class
-    constructor({ name, weekdays, freq, idLength }: { name: string; weekdays: Weekdays; freq: Freq; idLength: number }) {
+    constructor({ name, weekdays, freq }: { name: string; weekdays: Weekdays; freq: Freq }) {
         this.name = name;
         this.weekdays = weekdays;
         this.freq = freq;
-        this.id = generateID(idLength);
-        this.lastSeen = new ISODate();
     };
 
-    get weekdaysStr() {
+    get weekdaysStr(): string {
         // Returns str with active weekdays
         let weekdaysStr = "";
         const WEEKDAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
@@ -44,7 +36,7 @@ export default class Dish {
         return weekdaysStr
     };
 
-    allWeekdays() {
+    allWeekdays(): boolean {
         // If all days are true/all days are false return true else false
         if (this.weekdays.every((day, index, arr) => {
             return day == arr[0];
@@ -53,5 +45,9 @@ export default class Dish {
         } else {
             return false;
         };
+    }
+
+    shouldOccurOn(weekday: number): boolean {
+        return this.weekdays[weekday];
     }
 };
